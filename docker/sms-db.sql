f3a659e2f4fb6f0e6890603aaad0a980901275fa
zviad <zviad.nemsadze@magticom.ge>
2022-07-04 09:50:47 +0400

    Added Database docker

--- docker/sms-db.sql
+++ docker/sms-db.sql
@@ -27,20 +27,20 @@ create table sms_queue(
 );
 
 insert into sms_queue(id, phone_number, send_date, message_id, delivery_date, param, sms_text, sender,  state_id, is_geo, no_delivery)
-values (23,'599443000',null,null,null,null,'vazrovneb mashasadame varsebob rene','test_a2p',1,0,0);
+values (31,'599443000',null,null,null,null,'vazrovneb mashasadame varsebob rene','test_a2p',1,0,0);
 
 insert into sms_queue(id, phone_number, send_date, message_id, delivery_date, param, sms_text, sender,  state_id, is_geo, no_delivery)
-values (24,'599443000',null,null,null,null,'zamtaria sicivea shemodgoma midis meshinia sicivea zamtarshi rom ' ||
+values (27,'599443000',null,null,null,null,'zamtaria sicivea shemodgoma midis meshinia sicivea zamtarshi rom ' ||
                                            'icis sharshan ufro sashineli civi qari qroda magram sharshan chemTan' ||
                                            ' iyav sul ar meshinoda zamTaria sicivea Semodgoma midismeSinia sicivisa', 'test_a2p',1,0,0);
 
 
 
 insert into sms_queue(id, phone_number, send_date, message_id, delivery_date, param, sms_text, sender,  state_id, is_geo, no_delivery)
-values (25,'599443000',null,null,null,null,'ვაზროვნებ მაშასადამე სდფსდსდ სასდასდ','test_a2p',1,1,0);
+values (29,'599443000',null,null,null,null,'ვაზროვნებ მაშასადამე სდფსდსდ სასდასდ','test_a2p',1,1,0);
 
 insert into sms_queue(id, phone_number, send_date, message_id, delivery_date, param, sms_text, sender,  state_id, is_geo, no_delivery)
-values (26,'599443000',null,null,null,null,'ზამთარია სიცივეა ცივი ქარი ' ||
+values (32,'599443000',null,null,null,null,'ზამთარია სიცივეა ცივი ქარი ' ||
                                            'ქრის მეშინია სიცივისა ზამთარში რომ იცის შარშან უფრო საშინელი' ||
                                            ' ცივი ქარი ქროდა', 'test_a2p',1,1,0);
 SELECT *from sms_queue order by id desc;
