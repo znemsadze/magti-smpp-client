e5be468279819eb29e56a789c7cd53562ae1ceee
zviad <zviad.nemsadze@magticom.ge>
2023-02-16 10:01:09 +0400

    Added Database docker

--- docker/sms-db.sql
+++ docker/sms-db.sql
@@ -32,7 +32,7 @@ do $$
     declare
         v_id int;
     BEGIN
-        v_id:=37;
+        v_id:=40;
         insert into sms_queue(id, phone_number, send_date, message_id, delivery_date, param, sms_text, sender,  state_id, is_geo, no_delivery)
         values (v_id,'599443000',null,null,null,null,'vazrovneb mashasadame varsebob rene','adjaraspo',1,0,0);
 --         v_id:=v_id+1;
